cb20a368ae05be60a058b5f60bda6de109dc61c5
Natie Rautenbach <nrautenbach@koneqt.com>
2017-09-12 13:14:27 +0000

    www Ado update
    git-svn-id: https://repo.koneqt.com/svn/wwwadogreen@212 dfdc0155-8ce5-42c7-a92c-40817317653e

---
+++ blog/blogpost.php
@@ -0,0 +1,103 @@
+<?php 
+    $pathConfig = $_SERVER['DOCUMENT_ROOT'];
+    $pathConfig .= '/phpTraining/environment/config.php';
+    include_once $pathConfig;
+    //    Get the AppDataId from the URL
+    $intAppDataId = filter_input(INPUT_GET, 'id', FILTER_VALIDATE_INT);
+     
+    $json_url = ENV_RSS . 'ajax/getCmsDataLF.php?c=WebPostAdo&i=' . $intAppDataId;
+    //First version loading into an array
+    $json = file_get_contents($json_url);
+    $array = json_decode($json);
+    
+    if(isset($array[2])) {
+            foreach ($array[2] as $value) {
+                    $strBlogId = $value->AppDataId;
+                    $strBlogStatus = $value->Status;
+                    $strBlogTitle = $value->Title;
+                    $strBlogSEODesc = $value->SEODescription;
+                    $strBlogSEOKeywords = $value->SEOKeywords;
+                    $strBlogAssignedTo = $value->AssignedToName;
+                    $strBlogOpenDate = $value->OpenDate;
+                    $strBlogSector = $value->Sector;
+                    $strBlogCategory = $value->Category;
+                    $strBlogCountry = $value->Country;
+                    $strBlogDescription = $value->Description;
+                    $strBlogImgId = $value->ImageUploadedId; 
+                    $strBlogThumbnailId = $value->ThumbnailId; 
+            }
+    }  
+
+    $strPageTitle = $strBlogTitle;
+    $strPageDescription = $strBlogSEODesc;
+    $strPageKeywords = $strSEOKeywords;
+    $strPageImg = $strBlogImgId;
+    $strPageAuthor = 'AdoGreen Africa Recruiment Agency';
+    
+    include_once ENV_ROOT . 'includes/header.php';
+?> 
+<div class="container-fluid breadcrumb">
+    <div class="row">
+            <div class="col-md-12">
+                <ol class="breadcrumb">
+                          <li><a href="/index.php">AdoGreen Recruitment Africa</a></li>
+                          <li><a href="/blog/index.php" >Blog</a></li>
+                          <li class="active"></li>
+                    </ol>
+            </div>
+    </div>
+</div>
+
+  <div class="container">
+    <div class="row">
+        <div class="col-md-4">
+            <img class="img-responsive" width="100%" src="<?php echo ENV_RSS . 'DMSimage.php?i=' . $strBlogImgId; ?>">
+        </div>
+        <div class="col-md-8 job-header">
+            <h1 class="h1"><?php echo $strBlogTitle;?></h1>
+            <p class="post-meta"><i>Date posted: <span><?php echo $strBlogOpenDate;?></i></p>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-9">
+                <?php echo $strBlogDescription; ?>
+        </div>
+        <div class="col-md-3">
+                <?php include ENV_ROOT . 'includes/Page_Sidebar.php'; ?>
+        </div>
+    </div>   
+        <div class="row">
+            <div class="col-md-12 addthis_inline_share_toolbox">
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-12 margin-20">
+                    <a rel="nofollow" href="<?php echo ENV_ROOTURL . 'referral.php' ;?>" class="button button-red btn-block">Refer Someone Now</a>
+                    <h3>Did you know that you can make commission referring someone if they get the job!</h3>
+                <hr>
+                <p>Please note that all referrals are confidential, so we will not disclose your name to your referral.</p>
+                <ul>
+                    <li>You are more than welcome to let them know that you have referred them.</li>
+                    <li>Referrals work on a first come first serve basis, so should we already be in contact with the person you have referred, or where someone else has already referred them for this position, we will let you know in the next week or two as we come to your referral information. Our system automatically contacts the person you have referred on our behalf.</li>
+                    <li>Should the person you have referred be in line and interested in this position, we will be submitting their application, and only upon CV submission, will we inform you that the process has started for them.</li>
+                </ul>
+                <p>&nbsp;</p>
+                <p>Commission pay-out is processed only after the candidate starts with the company where we will then require your banking details.</p>
+
+            </div>
+        </div>
+       
+    
+</div>
+
+
+<?php    
+     
+    include ENV_ROOT . 'includes/footer.php';
+    include ENV_ROOT . 'includes/javascripts_basic.html';
+?>
+
+<script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-4d6e96a310897697"></script>
+
+</body>
+</html>
