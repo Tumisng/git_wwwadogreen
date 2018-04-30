deb9c2dde8b35a2462d106a9e963148349a566f3
LAPTOP-30ICRHD7\Mou <mouthabang@yahoo.com>
2018-04-30 15:01:16 +0200

    Addition of Flag images to Job-board

--- jobs-africa/job-board.php
+++ jobs-africa/job-board.php
@@ -1,205 +1,249 @@
 <?php
-    $strEnvConfigFile = '../environment/config.php';
-    $intLevel = 0;
-    while (!file_exists($strEnvConfigFile)) {
-       $strEnvConfigFile = '../' . $strEnvConfigFile;
-       $intLevel++;
-       // Iterate up 4 levels before giving up - this should never happen!
-       if ($intLevel > 3) {
-           die('Fatal error - environment configuration file could not be located');
-       }
+$strEnvConfigFile = '../environment/config.php';
+$intLevel = 0;
+while (!file_exists($strEnvConfigFile))
+{
+    $strEnvConfigFile = '../' . $strEnvConfigFile;
+    $intLevel++;
+    // Iterate up 4 levels before giving up - this should never happen!
+    if ($intLevel > 3)
+    {
+        die('Fatal error - environment configuration file could not be located');
     }
-    include_once $strEnvConfigFile;
+}
+include_once $strEnvConfigFile;
 
-    //include_once '../includes/header.php';
+//include_once '../includes/header.php';
 
-    $strPageTitle = 'AdoGreen Africa Job Board Heavy Engineering';
-    $strPageDescription = 'AdoGreen Africa Job Board - Specialist Recruitment - Heavy Engineering, Mining, Renewable Energy, , Power, Building And Construction';
-    $strPageKeywords ='AdoGreen Africa Job Board - Specialist Recruitment - Heavy Engineering, Mining, Renewable Energy, Power, Building And Construction';
-    $strPageAuthor = 'AdoGreen Africa Recruiment Agency';
+$strPageTitle = 'AdoGreen Africa Job Board Heavy Engineering';
+$strPageDescription = 'AdoGreen Africa Job Board - Specialist Recruitment - Heavy Engineering, Mining, Renewable Energy, , Power, Building And Construction';
+$strPageKeywords = 'AdoGreen Africa Job Board - Specialist Recruitment - Heavy Engineering, Mining, Renewable Energy, Power, Building And Construction';
+$strPageAuthor = 'AdoGreen Africa Recruiment Agency';
 
-            include_once ENV_ROOT . 'includes/header.php';
+include_once ENV_ROOT . 'includes/header.php';
 
-function fill_buttons_with_sector($connect)  {
-        $json_url = ENV_RSS . 'ajax/getCmsDataLF.php?c=Lookup&Cabinet=Job&Field=Sector';
-        $json1 = file_get_contents($json_url);
-        $array = json_decode($json1);
-        $arrResult = array();
-        $output = '';
+function fill_buttons_with_sector($connect)
+{
+    $json_url = ENV_RSS . 'ajax/getCmsDataLF.php?c=Lookup&Cabinet=Job&Field=Sector';
+    $json1 = file_get_contents($json_url);
+    $array = json_decode($json1);
+    $arrResult = array();
+    $output = '';
 
     $row = '';
-        if(isset($array[2])) {
-          foreach ($array[2] as $value)
-            {
-             $strSector = $value->DisplayValue;
-             array_push($arrResult,$strSector);
-          }
-        }
-
-        sort($arrResult);
-        foreach ($arrResult as $row)
+    if (isset($array[2]))
+    {
+        foreach ($array[2] as $value)
         {
-
-          $output .= '<div class="col-md-3"><input class="btn btn-block button-green" id = "sectorBtn" name = "sectorBtn"  onclick= "myFunction(this)"  type = "submit" value="' . $row . '" ></input></div>';
+            $strSector = $value->DisplayValue;
+            array_push($arrResult, $strSector);
         }
-        return $output;
- }
+    }
+
+    sort($arrResult);
+    foreach ($arrResult as $row)
+    {
+
+        $output .= '<div class="col-md-3"><input class="btn btn-block button-green" id = "sectorBtn" name = "sectorBtn"  onclick= "myFunction(this)"  type = "submit" value="' . $row . '" ></input></div>';
+    }
+    return $output;
+}
 
 // Fill the Jobs
- function fill_job($connect)  {
-        $output = '';
-        $json_url = ENV_RSS . 'ajax/getCmsDataLF.php?c=Job&Status=open&Published=yes';
-        $json1 = file_get_contents($json_url);
-        $array = json_decode($json1);
+function fill_job($connect)
+{
+    $output = '';
+    $json_url = ENV_RSS . 'ajax/getCmsDataLF.php?c=Job&Status=open&Published=yes';
+    $json1 = file_get_contents($json_url);
+    $array = json_decode($json1);
 
-        if(isset($array[2])) {
-            foreach ($array[2] as $value) {
-               $strAppDataId = $value->AppDataId;
-               $strJobTitle = $value->JobTitle;
-               $strJobContract = $value->ContractType;
-               $strJobShortDesc = $value->ShortDescription;
-               $strJobOpenDate = $value->OpenDate;
-               $strJobSector = $value->Sector;
-               $strJobLocation = $value->Location;
-               $strJobCountry = $value->Country;
-          //    replace %20 in the URL
-               $strJobTitleURL=str_ireplace(" ","-",$strJobTitle);
-               $output .= '<li>
+    if (isset($array[2]))
+    {
+        foreach ($array[2] as $value)
+        {
+            $strAppDataId = $value->AppDataId;
+            $strJobTitle = $value->JobTitle;
+            $strJobContract = $value->ContractType;
+            $strJobShortDesc = $value->ShortDescription;
+            $strJobOpenDate = $value->OpenDate;
+            $strJobSector = $value->Sector;
+            $strJobLocation = $value->Location;
+            $strJobCountry = $value->Country;
+            //    replace %20 in the URL
+            $strJobTitleURL = str_ireplace(" ", "-", $strJobTitle);
+            $output .= '<li>
                                         <div class="card-jobs card-green">
-                                                <a href="' . ENV_ROOTURL . 'jobs-africa/job-details.php?id=' . $strAppDataId . '&name=' . $strJobTitleURL .'">
+                                                <a href="' . ENV_ROOTURL . 'jobs-africa/job-details.php?id=' . $strAppDataId . '&name=' . $strJobTitleURL . '">
                                                         <div class="card-content">
-                                                            <h6 class="category pull-right hidden-xs">'. $strJobSector . ' / ' . $strJobLocation . ' / ' . $strJobCountry .'</h6>
-                                                            <h4 class="title">'. $strJobTitle .'</h4>
+                                                            <h6 class="category pull-right hidden-xs">' . $strJobSector . ' / ' . $strJobLocation . ' / ' . $strJobCountry . '</h6>
+                                                            <h4 class="title">' . $strJobTitle . '</h4>
                                                             <p class="description hidden-xs">' . $strJobShortDesc . '</p>
                                                         </div>
                                                         <div>
-                                                        <p class="hidden-lg hidden-md hidden-sm" ><small >'. $strJobSector . ' / ' . $strJobLocation . ' / ' . $strJobCountry .'</small></p>
+                                                        <p class="hidden-lg hidden-md hidden-sm" ><small >' . $strJobSector . ' / ' . $strJobLocation . ' / ' . $strJobCountry . '</small></p>
                                                         </div>
                                                 </a>
                                         </div>
                                 </li>';
-                }
         }
-        return $output;
- }
+    }
+    return $output;
+}
+
 // Fill Country
- function get_country()
- {
-        $output = '';
-        $json_url = ENV_RSS . 'ajax/getCmsDataLF.php?c=Job&Status=open&Published=yes&f=Country';
-        $json1 = file_get_contents($json_url);
-        $array = json_decode($json1);
-        $arrResult = [];
-        $found = false;
+function get_country()
+{
+    $output = '';
+    $json_url = ENV_RSS . 'ajax/getCmsDataLF.php?c=Job&Status=open&Published=yes&f=Country';
+    $json1 = file_get_contents($json_url);
+    $array = json_decode($json1);
+    $arrResult = [];
+    $found = false;
 
-        if(isset($array[2]))
+    if (isset($array[2]))
+    {
+        foreach ($array[2] as $value)
         {
-          foreach ($array[2] as $value)
-          {
-             $strCountry = $value->Country;
-             array_push($arrResult,$strCountry);
-          }
+            $strCountry = $value->Country;
+            array_push($arrResult, $strCountry);
         }
-        sort($arrResult);
+    }
+    sort($arrResult);
 
-        $line = array_unique($arrResult);
+    $line = array_unique($arrResult);
 
-        foreach ($line as $row)
-        {
-          $output .= '<input class="btn btn-block button-green" id = "sectorBtn" name = "sectorBtn"  onclick= "function_fill_by_country(this)"  type = "submit" value="' . $row . '" ></input>';
+    foreach ($line as $row)
+    {
+        $countryNameWithOutSpaces = str_replace(' ', '', $row);
+
+        $output .= '<input class="btn btn-block button-green" id = "sectorBtn" name = "sectorBtn"  onclick= "function_fill_by_country(this)"  type = "image" src='.'../assets/img/'.$countryNameWithOutSpaces.'.gif value="'.'' . $row . '" >'.$row.' </input>';
+        
         }
-        return $output;
- }
+    return $output;
+}
+?>
+<div class="container">
 
- ?>
-        <div class="container">
-            <div class="row padding-bottom-20">
-                    <?php echo fill_buttons_with_sector($connect); ?>
-            </div>
-            <div class="row padding-bottom-20">
-                <div class="col-md-12">
-                    <h3>What Job Are You Looking For?</h3>
-                    <input type="text" id="search-input" placeholder="Search - Job Names Or Country">
-                </div>
-             </div>
-             </br>
+    <div>
+        
+    </div>
+    <div class="row padding-bottom-20">
+<?php echo fill_buttons_with_sector($connect); ?>
+    </div>
+    <div class="row padding-bottom-20">
+        <div class="col-md-12">
+            <h3>What Job Are You Looking For?</h3>
+            <input type="text" id="search-input" placeholder="Search - Job Names Or Country">
         </div>
-        <div class="container">
-            <div class="col-md-2">
-            </div>
-            <div class="col-md-8" id="show_sector">
-                <ul class="list-unstyled joblist" id="job-board-listing">
-                         <?php echo fill_job($connect);?>
-                        </ul>
-            </div>
-            <div class="col-md-2">
-            Countries:
-                </br> <?php echo get_country(); ?>
-            </div>
-        </div>
+    </div>
+    </br>
+</div>
+<div class="container">
+    <div class="col-md-2">
+    </div>
+    <div class="col-md-8" id="show_sector">
+        <ul class="list-unstyled joblist" id="job-board-listing">
+<?php echo fill_job($connect); ?>
+        </ul>
+    </div>
+    <div class="col-md-2">
+        Countries:
+        </br> <?php echo get_country(); ?>
+    </div>
+</div>
 <?php
-    include ENV_ROOT . 'includes/footer.php';
-    include ENV_ROOT . 'includes/js_scripts.php';
+include ENV_ROOT . 'includes/footer.php';
+include ENV_ROOT . 'includes/js_scripts.php';
 ?>
 
-    <script>
-        $("#search-input").on("keyup", function () {
+<script>
+    $("#search-input").on("keyup", function () {
         var search = $(this).val().trim().toLowerCase();
         $(".card-jobs").show().filter(function () {
-        return $(this).text().toLowerCase().indexOf(search) < 0;
+            return $(this).text().toLowerCase().indexOf(search) < 0;
         }).hide();
+    });
+</script>
+
+<script>
+    $(document).ready(function () {
+        $('#sector').change(function () {
+            var sector = $(this).val();
+            $.ajax({
+                url: "load_data.php",
+                method: "POST",
+                data: {sector, sector},
+                success: function (data) {
+                    $('#show_sector').html(data);
+                }
+            });
         });
-    </script>
+    });
+</script>
+<script>
+    function myFunction(objSectorName)
+    {
 
-     <script>
-        $(document).ready(function(){
-             $('#sector').change(function(){
-                  var sector = $(this).val();
-                  $.ajax({
-                       url:"load_data.php",
-                       method:"POST",
-                       data:{sector,sector},
-                       success:function(data){
-                            $('#show_sector').html(data);
-                       }
-                  });
-             });
+        var sector = objSectorName.value;
+        $.ajax({
+            url: "load_data.php",
+            method: "POST",
+
+            data: {sector, sector},
+            success: function (data) {
+                $('#show_sector').html(data);
+            }
         });
-     </script>
-     <script>
-          function myFunction(objSectorName)
-          {
+    }
+</script>
 
-         var sector = objSectorName.value;
-                  $.ajax({
-                       url:"load_data.php",
-                       method:"POST",
+<script>
+    function function_fill_by_country(objCountryName)
+    {
 
-                       data:{sector,sector},
-                       success:function(data){
-                            $('#show_sector').html(data);
-                       }
-                  });
-          }
-     </script>
-     
-     <script>
-          function function_fill_by_country(objCountryName)
-          {
+        var country = objCountryName.value;
+        $.ajax({
+            url: "load_data.php",
+            method: "POST",
 
-         var country = objCountryName.value;
-                  $.ajax({
-                       url:"load_data.php",
-                       method:"POST",
+            data: {country, country},
+            success: function (data) {
+                $('#show_sector').html(data);
+            }
+        });
+    }
+</script>
+<script>
 
-                       data:{country,country},
-                       success:function(data){
-                            $('#show_sector').html(data);
-                       }
-                  });
-          }
-     </script>
-    </body>
+    $(document).ready(function() {
+    $('#searchterm').on('submit', function(e) {
+    //Stop the form from submitting itself to the server.
+    e.preventDefault();
+            var searchwords = $('#searchwords').val();
+            var jobid = $('#job').val();
+            if (jobid != '')
+    {
+
+    $('#loadingmessage').show();
+            console.log(jobid);
+            console.log(searchwords);
+            $.ajax({
+            type: "POST",
+                    dataType: 'json',
+                    url: 'ajax/Recruitment/getRecruitAdvancedSearch.php',
+                    data: {searchwords: searchwords, jobid: jobid},
+                    success: function(data) {
+                    console.log(data);
+                            $('#loadingmessage').hide();
+                            $('#searchresult').dataTable({
+
+                    }
+                    }
+                    };
+                    }
+
+</script>
+</body>
 </html>
 
 
