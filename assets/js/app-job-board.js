f11b3deddf91b9be7a9d0ac6fb63c84ba3578f50
Natie <nrautenbach@koneqt.com>
2018-11-15 14:38:44 +0200

    Job Board Ajax js upgrade

--- assets/js/app-job-board.js
+++ assets/js/app-job-board.js
@@ -6,22 +6,10 @@
  * (c) AdoGreen Africa 2018
  */
 $(function () {
-    $("#search-input").focus();
 
     loadAllSectors();
     loadOpenJobs();
 
-    $('#sector').change(function () {
-        var sector = $(this).val();
-        $.ajax({
-            url: "load_data.php",
-            method: "POST",
-            data: {sector, sector},
-            success: function (data) {
-                $('#show_sector').html(data);
-            }
-        });
-    });
 });
 
 //Search bar inputs
@@ -31,6 +19,22 @@ $("#search-input").on("keyup", function () {
         return $(this).text().toLowerCase().indexOf(search) < 0;
     }).hide();
 });
+
+
+//$('#sector').change(function () {
+//
+//    var sector = $(this).val();
+//
+//    $.ajax({
+//        url: "load_data.php",
+//        method: "POST",
+//        data: {sector, sector},
+//        success: function (data) {
+//            $('#show_sector').html(data);
+//        }
+//    });
+//
+//});
 
 $(function() {
    'use strict';
@@ -45,54 +49,6 @@ $(function() {
    });
 });
 
-
-
-function myFunction(objSectorName)
-{
-    var sector = objSectorName.value;
-    $.ajax({
-        url: "load_data.php",
-        method: "POST",
-
-        data: {sector:sector},
-        success: function (data) {
-            $('#show_sector').html(data);
-        }
-    });
-}
-
-function function_fill_by_country(objCountryName)
-{
-    var country = objCountryName.value;
-
-    $.ajax({
-        url: "load_data.php",
-        method: "POST",
-
-        data: {country:country},
-        success: function (data) {
-            $('#show_sector').html(data);
-        }
-    });
-}
-
-////Loading of Jobs - AJAX on page startup
-//function loadAllJobs() {
-//    var all;
-//
-//    $.ajax({
-//        url: "load_data.php",
-//        method: "POST",
-//
-//        data: {all:1},
-//        success: function (data) {
-////            console.log(data);
-//            $('#job-board-listing').html(data);
-//        }
-//    });
-//}
-
-//TODO:NATie Make this work
 function loadOpenJobs() {
 
     $.ajax({
@@ -101,7 +57,79 @@ function loadOpenJobs() {
         datatype: "json",
 
         success: function (data) {
-            console.log(data);
+//          console.log(data);
+
+            loadJobs(data);
+
+        }
+    });
+}
+
+function changeSector(objSectorName)
+{
+    var sector = objSectorName.value;
+
+    $.ajax({
+        url: KQ_Url + "/ajax/Recruitment/getOpenJobs.php?Sector=" + sector,
+        method: "POST",
+        datatype: "json",
+
+        success: function (data) {
+
+            loadJobs(data,"Selected");
+
+        }
+    });
+}
+
+function fillCountry(objCountryName){
+
+//
+    var country = objCountryName.value;
+
+    $.ajax({
+        url: KQ_Url + "/ajax/getCmsDataLF.php?c=Job&s=OpenDate&Status=open&Published=yes&Country=" + country,
+        method: "POST",
+        datatype: "json",
+
+        success: function (data) {
+
+        loadJobs(data,"Selected");
+
+        }
+    });
+}
+
+function loadAllSectors() {
+
+    $.ajax({
+        url: KQ_Url + "/ajax/getCmsDataLF.php?c=Lookup&Cabinet=Job&Field=Sector",
+        method: "POST",
+        datatype: "json",
+
+        success: function (data) {
+//            console.log(data);
+//          Move only values from Array [2] into a var
+            var objSectors = data[2];
+            objSectors.sort(sortArray);
+//            Cycle through objSectors which holds the actual values
+            var returnHtml ='';
+            returnHtml += '<h3>Sectors:</h3>';
+
+            for (var i = 0; i < objSectors.length; i++) {
+                returnHtml += '<div class="col-md-12"><input class="btn btn-block button-green" id = "sectorBtn" name = "sectorBtn"  onclick= "changeSector(this)"  type = "submit" value="' +  objSectors[i]['DisplayValue'] + '" ></input></div>'
+
+//                console.log(objSectors[i]['DisplayValue']);
+
+            }
+//            console.log(returnHtml);
+            $('#sector_container').html(returnHtml);
+        }
+    });
+}
+
+function loadJobs(data,loadAll){
+console.log(data);
 //          Move only values from Array [2] into a var
             var objJobs = data[2];
 
@@ -145,49 +173,24 @@ function loadOpenJobs() {
             returnHtmlCountry = '<h3>Choose Country:</h3>';
 
             for (var i = 0; i < objCountry.length; i++) {
-                console.log(objCountry[i]);
+//                console.log(objCountry[i]);
                 var picFlag = objCountry[i].replace(/\s/g, '');
-                returnHtmlCountry += '<div class="col-md-3"><input class="img-flag-icon" id = "sectorBtn" name = "sectorBtn"  onclick= "function_fill_by_country(this)"  type = "image" src="' + Ado_Url + '/assets/img/flags/' + picFlag + '.svg" value="' +objCountry[i] + '" ></input></div>';
+                returnHtmlCountry += '<div class="col-md-3"><input class="img-flag-icon" id = "sectorBtn" name = "sectorBtn"  onclick= "fillCountry(this)"  type = "image" src="' + Ado_Url + '/assets/img/flags/' + picFlag + '.svg" value="' +objCountry[i] + '" ></input></div>';
 
-                console.log(returnHtmlCountry);
 
+
+            }
+            //                console.log(returnHtmlCountry);
+            if (loadAll === "Selected") {
+                //    Button to load all the jobs again
+                returnHtmlCountry += '<div class="col-md-3"><button class="btn btn-info" onclick= "loadOpenJobs()">Load All Jobs</button></div>';
             }
 
 //           console.log(objCountry);
             $('#country_container').html(returnHtmlCountry);
             $('#job-board-listing').html(returnHtml);
-        }
-    });
+            $("#search-input").focus();
 }
-
-function loadAllSectors() {
-
-    $.ajax({
-        url: KQ_Url + "/ajax/getCmsDataLF.php?c=Lookup&Cabinet=Job&Field=Sector",
-        method: "POST",
-        datatype: "json",
-
-        success: function (data) {
-//            console.log(data);
-//          Move only values from Array [2] into a var
-            var objSectors = data[2];
-            objSectors.sort(sortArray);
-//            Cycle through objSectors which holds the actual values
-            var returnHtml ='';
-            returnHtml += '<h3>Sectors:</h3>';
-
-            for (var i = 0; i < objSectors.length; i++) {
-                returnHtml += '<div class="col-md-12"><input class="btn btn-block button-green" id = "sectorBtn" name = "sectorBtn"  onclick= "myFunction(this)"  type = "submit" value="' +  objSectors[i]['DisplayValue'] + '" ></input></div>'
-
-//                console.log(objSectors[i]['DisplayValue']);
-
-            }
-//            console.log(returnHtml);
-            $('#sector_container').html(returnHtml);
-        }
-    });
-}
-
 
 function sortArray(a, b) {
     if (a['DisplayValue'] === b['DisplayValue']) {
