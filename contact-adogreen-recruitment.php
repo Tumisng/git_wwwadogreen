5a7986f301fc72468dbe31a0f5d497cd1b0ed213
LAPTOP-30ICRHD7\Mou <mouthabang@yahoo.com>
2019-01-08 08:20:05 +0200

    Contact page update.

--- contact-adogreen-recruitment.php
+++ contact-adogreen-recruitment.php
@@ -1,10 +1,10 @@
 <?php
-    $strEnvConfigFile = 'environment/config.php';
-    include_once $strEnvConfigFile;
+$strEnvConfigFile = 'environment/config.php';
+include_once $strEnvConfigFile;
 
-    include_once ENV_ROOT . 'includes/header.php';
-    ?>
- <div class="container-fluid breadcrumb">
+include_once ENV_ROOT . 'includes/header.php';
+?>
+<div class="container-fluid breadcrumb">
     <div class="row">
         <div class="container">
             <div class="col-md-12">
@@ -22,48 +22,46 @@
             <hr>
         </div>
     </div>
-    <div class="row margin-20">
-        <div class="col-md-5">
-                <div class="headline"><h2>Contact Details</h2></div>
-                <ul class="list-unstyled who margin-bottom-30">
-                    <li> <h3> <i class="fa fa-home"></i> South Africa Head Office</li></h3>
-                    <li><a href="#"><i class="fa fa-envelope"></i> info@adogreen.com</a></li>
-                    <li><a href="#"><i class="fa fa-phone"></i> +27 10 140 0951</a></li>
-                    <li><a href="http://www.adogreen.com"><i class="fa fa-globe"></i> http://www.adogreen.com</a></li>
-                </ul>
-                </br>
-                <ul class="list-unstyled who margin-bottom-30">
-                    <li>Industrial Assessments</li>
-                    <h3><li> <i class="fa fa-home"></i> Rustenburg</h3>
-                        Multiprac Building </br>
-                        167A Church Street </li>
-                </ul>
-
-                <ul class="list-unstyled who margin-bottom-30">
-                    <li><h3><i class="fa fa-home"></i>Botswana - AdoGreen Botswana HQ</h3>
-                        Plot 47 Unit 9 </br>
-                        Gaborone International Commerce Park </li>
-                </ul>
-                </br>
-
-
+    <div class="row">
+        <div class="col-md-4">
+            <div class="headline"><h2 >Contact Details</h2></div>
+            <h3><img hspace="10" src="<?php echo ENV_ROOTURL; ?>assets/img/icons/flat/building.png">  South Africa Head Office</h3>
+            <ul class="spacing-25">
+                <li ><a href="mailto:info@adogreen.com"><img hspace="20" src="<?php echo ENV_ROOTURL; ?>assets/img/icons/flat/mail.png">info@adogreen.com</a></li>
+                <li ><a href="tel:+27 10 140 0951"><img hspace="20" src="<?php echo ENV_ROOTURL; ?>assets/img/icons/flat/call.png"> +27 10 140 0951</a></li>
+                <li ><a href="http://www.adogreen.com"><img hspace="20" src="<?php echo ENV_ROOTURL; ?>assets/img/icons/flat/web.png">http://www.adogreen.com</a></li>
+            </ul>
+         
+            
         </div>
         <div class="col-md-4">
-                <!-- Business Hours -->
-                <div class="headline"><h2>Business Hours +2GMT</h2></div>
-                <ul class="list-unstyled margin-bottom-30">
-                    <li><strong>Monday-Friday:</strong> 8:00am to 4:30pm</li>
-                    <li><strong>Saturday:</strong> Closed</li>
-                    <li><strong>Sunday:</strong> Closed</li>
-                </ul>
+            <div class="headline"><h2 >Address details</h2></div>
+           <ul class="list-unstyled indent-50">
+                <h3 class="text-left"><img hspace="10" src="<?php echo ENV_ROOTURL; ?>assets/img/icons/flat/building2.png">Rustenburg</h3>
+                <p>Multiprac Building</p>
+                <p>167A Church Street</p>
+            </ul>
+            <ul class="list-unstyled indent-50">
+                <h3 class="text-left"><img hspace="10" src="<?php echo ENV_ROOTURL; ?>assets/img/icons/flat/building2.png">AdoGreen Botswana HQ</h3>
+                     <p>Plot 47 Unit 9 </p>
+                     <p>Gaborone International Commerce Park </p>
+            </ul>
         </div>
-        <div class="col-md-3">
+        
+        <div class="col-md-4">
+            <!-- Business Hours -->
+            <div class="headline"><h2>Business Hours +2GMT</h2></div>
+            <ul class="list-unstyled spacing-10">
+                <li><strong>Monday - Friday:</strong> 8:00am to 4:30pm</li>
+                <li><strong>Saturday:</strong> Closed</li>
+                <li><strong>Sunday:</strong> Closed</li>
+            </ul>
 
             <div>
-                <a class="btn btn-block btn-danger" href="<?php echo ENV_ROOTURL;?>client-register-job.php">Need Staff?</a>
+                <a class="btn btn-block btn-danger" href="<?php echo ENV_ROOTURL; ?>client-register-job.php">Need Staff?</a>
             </div>
             <div>
-                <a class="btn btn-block btn-success" href="<?php echo ENV_ROOTURL;?>jobs-africa/register-cv.php">Need A Job?</a>
+                <a class="btn btn-block btn-success" href="<?php echo ENV_ROOTURL; ?>jobs-africa/register-cv.php">Need A Job?</a>
             </div>
             <hr>
 
@@ -84,9 +82,14 @@
                 <span class="fa fa-youtube"></span>
             </a>
 <!--                <a href="https://www.linkedin.com/company/adogreen"><img class="img-responsive" src="assets/img/social-linkedin.png" width="100%"></a>-->
-            </div>
+
+
+
+
         </div>
-    <!-- ./row -->
+        <!-- ./row -->
+
+    </div>
     <div class="row">
         <div class="col-md-4">
             <h3>Yvette Rautenbach</h3>
@@ -96,7 +99,7 @@
         <div class="col-md-4">
             <h3>Dr. Amanda Agathagelou</h3>
             <p>Psychologist - Industrial | Consulting</p>
-            <img class="img-responsive" src="assets/img/social-linkedin-amanda.png" width="100%" alt="Dr. Amanda Agathagelou Linkedin Heavy Industry Pshycologist Africa">
+            <a href="https://www.linkedin.com/in/dr-amanda-agathagelou-399a4977/"><img class="img-responsive" src="assets/img/social-linkedin-amanda.png" width="100%" alt="Dr. Amanda Agathagelou Linkedin Heavy Industry Pshycologist Africa"></a>
         </div>
         <div class="col-md-4">
             <h3>Neo Nwako</h3>
@@ -104,20 +107,30 @@
             <a href="https://www.linkedin.com/in/neo-nwako-a7bb166/"><img class="img-responsive" src="assets/img/social-linkedin-neo.png" width="100%" alt="Neo Nwako Director Botswana"></a>
 
         </div>
-     </div>
-     <div class="row">
+    </div>
+    <div class="row">
         <div class="col-md-4">
             <h3>Natie Rautenbach</h3>
             <p>Director | CIO AdoGreen Africa</p>
             <a href="https://www.linkedin.com/in/natierau/"><img class="img-responsive" src="assets/img/social-linkedin-natie.png" width="100%" alt="Natie Rautenbach CIO Africa"></a>
 
         </div>
+        <div class="col-md-4">
+            <h3>Richard Wadikgosi</h3>
+            <p>Senior Recruitment Coordinator | Botswana </p>
+            <a href="https://www.linkedin.com/in/richwadikgosi/"><img class="img-responsive" src="assets/img/social-linkedin-Richard.jpg" width="100%" alt="Richard Wadikgosi Senior Recruitment Coordinator | Botswana"></a>
+
+        </div>
     </div>
+
+
+
+
 
 </div>
 <?php
-    include ENV_ROOT . 'includes/footer.php';
-    include ENV_ROOT . 'includes/js_scripts.php';
+include ENV_ROOT . 'includes/footer.php';
+include ENV_ROOT . 'includes/js_scripts.php';
 ?>
 
 
