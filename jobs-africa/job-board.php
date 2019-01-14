9cb177c04898a331acfcc3afaa47fcc81bb42917
LAPTOP-30ICRHD7\Mou <mouthabang@yahoo.com>
2019-01-14 09:16:09 +0200

    image and icon update

--- jobs-africa/job-board.php
+++ jobs-africa/job-board.php
@@ -2,22 +2,22 @@
 // =============================================================================
 //  basic job board for AdoGreen
 //
-//  Author: Natie Rautenbach + Upgraded by Mou Mokete
+//  Author: Natie Rautenbach + Upgraded by Mou Thabang
 //  Date written: 03/10/2017
 //  (c) AdoGreen Africa / The Missing Floor
 //==============================================================================
 $strEnvConfigFile = '../environment/config.php';
 $intLevel = 0;
 while (!file_exists($strEnvConfigFile))
-{
+    {
     $strEnvConfigFile = '../' . $strEnvConfigFile;
     $intLevel++;
     // Iterate up 4 levels before giving up - this should never happen!
     if ($intLevel > 3)
-    {
+        {
         die('Fatal error - environment configuration file could not be located');
+        }
     }
-}
 include_once $strEnvConfigFile;
 
 $strPageTitle = 'AdoGreen Africa Job Board Heavy Engineering';
@@ -26,29 +26,35 @@ $strPageKeywords = 'AdoGreen Africa Job Board - Specialist Recruitment - Heavy E
 $strPageAuthor = 'AdoGreen Africa Recruiment Agency';
 
 include_once ENV_ROOT . 'includes/header.php';
-
 ?>
 <div class="container">
-    <div class="row padding-bottom-20" id="country_container">
-
-    </div>
     <div id='loadingmessage' style='display:none' class="text-center row padding-bottom-20">
-        <img src='<?php echo ENV_ROOTURL;?>assets/img/ajax-loader.gif'/>
+        <img src='<?php echo ENV_ROOTURL; ?>assets/img/ajax-loader.gif'/>
     </div>
     <div class="row padding-bottom-20">
-        <style>input[type=text] {
-            width: 130px;
-            -webkit-transition: width 0.4s ease-in-out;
-            transition: width 0.6s ease-in-out;
-        }
-
-        /* When the input field gets focus, change its width to 100% */
-        input[type=text]:focus {
-            width: 100%;
-        }</style>
-        <div class="col-md-12">
-            <h3>What Job Are You Looking For?</h3>
-            <input type="text" id="search-input" placeholder="Search - Job Names Or Country">
+        <div class="col-md-12 text-center">
+            <h3 class="text-center">What Job Are You Looking For?</h3>
+            <input type="searching" id="search-input" placeholder="Search"><br><br>
+        </div>
+    </div>
+    <div class="row padding-bottom-20">
+        <div class="col-md-3 text-center">
+            <div class="country-dropdown">
+                <span class="selLabel">Choose country</span>
+                <input type="hidden" name="cd-dropdown">
+                <ul class="country-dropdown-list" id="listCountries">
+                </ul>
+            </div>
+        </div>
+        <div class="col-md-3 text-center" >
+            <div class="sector-dropdown">
+                <span class="setLabel-Sector">Choose Sector</span>
+                <input type="hidden" name="cd-dropdown">
+                <ul class="sector-dropdown-list" id="listSectors">
+                </ul>
+            </div>
+        </div>
+        <div class="col-md-6 text-center" >
         </div>
     </div>
     </br>
@@ -64,19 +70,17 @@ include_once ENV_ROOT . 'includes/header.php';
 
 </style>
 <div class="container">
-    <div class="col-md-10" id="show_sector">
-        <ul class="list-unstyled joblist" id="job-board-listing">
-
-        </ul>
-    </div>
-    <div class="col-md-2" id="sector_container">
-
+    <div class="row">
+        <div class="col-md-12" id="show_sector">
+            <ul class="list-unstyled joblist" id="job-board-listing">
+            </ul>
+        </div>
     </div>
 </div>
 <?php
 include ENV_ROOT . 'includes/footer.php';
 include ENV_ROOT . 'includes/js_scripts.php';
-echo '<script src="' . ENV_ROOTURL . 'assets/js/app-job-board.js"> </script>';
+echo '<script src="' . ENV_ROOTURL . 'assets/js/app-job-board.js"></script>';
 ?>
 
 </body>
